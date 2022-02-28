028e85ed0d20399eabe69e51faa28a46c35110c2
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-02-28 17:43:04 +0000

    disabled static files

--- server/djangoBackend/settings.py
+++ server/djangoBackend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'majorProject',
     'rest_framework',
     'corsheaders',
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'djangoBackend.wsgi.application'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1.3000'
+
 ]
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -129,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
