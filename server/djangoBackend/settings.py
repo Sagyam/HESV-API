ef43648540a2bc69716249c5fd7aeea7302d27f6
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-02-28 20:04:21 +0000

    changed port to 8k

--- server/djangoBackend/settings.py
+++ server/djangoBackend/settings.py
@@ -75,8 +75,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangoBackend.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1.3000'
+    'http://localhost:8000',
+    'http://127.0.0.1.8000'
 
 ]
 # Database
