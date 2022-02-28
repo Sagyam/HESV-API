8f4f9ca44f39d7111b21245c8a494e0c929faa6a
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-02-28 13:42:51 +0545

    Refactored Routing

--- djangoBackend/majorProject/views.py
+++ djangoBackend/majorProject/views.py
@@ -6,10 +6,30 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['POST'])
-def three_degree_linear_equation(request):
-    eq1 = request.data['eq1']
-    eq2 = request.data['eq2']
-    eq3 = request.data['eq3']
+def get_polynomial_equation(request):
+    image = request.data['image']
+    data = {
+        'eqn': 'x3+y3+z3',
+        'image_name': image.name
+    }
+    return Response(data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def get_linear_equation(request):
+    image = request.data['image']
+    data = {
+        'equation': 'x2+y2',
+        'name': image.name
+    }
+    return Response(data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def solve_3d_linear_equation(request):
+    eq1 = request.data['equation1']
+    eq2 = request.data['equation2']
+    eq3 = request.data['equation3']
     data = {
         'x': 1,
         'y': 2,
@@ -20,9 +40,9 @@ def three_degree_linear_equation(request):
 
 
 @api_view(['POST'])
-def two_degree_linear_equation(request):
-    eq1 = request.data['eq1']
-    eq2 = request.data['eq2']
+def solve_2d_linear_equation(request):
+    eq1 = request.data['equation1']
+    eq2 = request.data['equation2']
     data = {
         'x': 1,
         'y': 2,
@@ -32,30 +52,10 @@ def two_degree_linear_equation(request):
 
 
 @api_view(['POST'])
-def get_equation_polynomial(request):
-    image = request.data['image']
-    data = {
-        'eqn': 'x3+y3+z3',
-        'image_name': image.name
-    }
-    return Response(data, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
-def solve_the_equation_polynomial(request):
-    equation = request.data['eq']
+def solve_polynomial_equation(request):
+    equation = request.data['equation']
     data = {
         'solution': [1, 2],
         'solution_type': 'linear'
-    }
-    return Response(data, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
-def get_two_degree_equation(request):
-    image = request.data['image']
-    data = {
-        'eq': 'x2+y2',
-        'name': image.name
     }
     return Response(data, status=status.HTTP_200_OK)
