1d7ecbcc830bc638f3a15e42151a4a7303a2c1d6
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-02-28 17:56:05 +0000

    Static files

--- server/djangoBackend/settings.py
+++ server/djangoBackend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'majorProject',
     'rest_framework',
     'corsheaders',
@@ -130,7 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
